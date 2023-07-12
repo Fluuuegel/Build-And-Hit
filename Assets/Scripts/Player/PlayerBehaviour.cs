fd35028fc364e98c1d32c7e72e9eced42eb5b2f5
fluegelcat <2292009362@qq.com>
2023-07-12 15:48:30 +0800

    .

---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : SingletonMonobehaviour<PlayerBehaviour>
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
