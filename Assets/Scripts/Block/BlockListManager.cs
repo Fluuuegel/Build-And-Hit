d5eb1b7bd912b173b90c344d20f3086293c0870f
RaymondHe <2224928976@qq.com>
2023-07-16 16:53:14 +0800

    加入了BGM、用于测试的技能：有0.2s的概率获得->消除敌人第一块方块

--- Assets/Scripts/Block/BlockListManager.cs
+++ Assets/Scripts/Block/BlockListManager.cs
@@ -19,9 +19,18 @@ public class BlockListManager : MonoBehaviour
         eBlue
     };
 
+    //For Skills
+    private enum BlockSkills
+    {
+        Normal,
+        Skills
+    };
+
     private BlockState mBlockState = BlockState.eIdle;
 
     private BlockColor mBlockColor = BlockColor.eRed;
+
+    private BlockSkills mBlockSkills = BlockSkills.Normal;
 
     public float hitSpeed = 2f;
     public bool isLocked = true;
@@ -93,6 +102,16 @@ public class BlockListManager : MonoBehaviour
 
     private void ServiceIdleState() {
         mBlockColor = (BlockColor)Random.Range(0, 3);
+        float randomSkill = Random.Range(0f, 1f);
+        if (randomSkill > 0.2f)
+        {
+            mBlockSkills = BlockSkills.Normal;
+        }
+        else
+        {
+            mBlockSkills = BlockSkills.Skills;
+            Debug.Log("Find Skill Block!");
+        }
         p1Animator = GameManager.sTheGlobalBehavior.GetPlayerManager().getPlayer1().GetComponent<PlayerBehaviour>().animator;
         p2Animator = GameManager.sTheGlobalBehavior.GetPlayerManager().getPlayer2().GetComponent<PlayerBehaviour>().animator;
         if (p1Turn) {
@@ -121,6 +140,35 @@ public class BlockListManager : MonoBehaviour
             mBlockState = BlockState.eSelectHit;
             Debug.Log("SelectHit");
             return ;
+        }
+
+
+        //Use skills
+        if (Input.GetKeyDown(KeyCode.P) && (mBlockSkills == BlockSkills.Skills))
+        {
+            Debug.Log("It's skill time!");
+            //temporarily: Destroy the first Block of enemy
+            {
+                mTargetBlockIndex = 1;
+                beHitBlock = mP1BlockManager.GetBlockAt(mP1BlockManager.GetHeight() - mTargetBlockIndex);
+                beHitBlockPos = beHitBlock.transform.position;
+                if (p1Turn)
+                {
+                    GameObject bullet = mP1BlockManager.GetBlockAt(mP1BlockManager.GetHeight() - 1);
+                    mP2BlockManager.BeingHitBlockDestroy(bullet, mP2BlockManager.GetHeight() - mTargetBlockIndex);//player 2被击打的玩家
+                    //mP1BlockManager.DestroyOneBlock(mP1BlockManager.GetHeight() - 1);//player 1: 当前的玩家
+                }
+                else
+                {
+                    GameObject bullet = mP2BlockManager.GetBlockAt(mP2BlockManager.GetHeight() - 1);
+                    mP1BlockManager.BeingHitBlockDestroy(bullet, mP1BlockManager.GetHeight() - mTargetBlockIndex);//player 1
+                    //mP2BlockManager.DestroyOneBlock(mP2BlockManager.GetHeight() - 1);//player 2: 当前的玩家
+                }
+                music.clip = Resources.Load<AudioClip>("music/Audio_Debuff");
+                music.Play();
+            }
+            mBlockSkills = BlockSkills.Normal;//Skills just use once
+            return;
         }
     }
 
