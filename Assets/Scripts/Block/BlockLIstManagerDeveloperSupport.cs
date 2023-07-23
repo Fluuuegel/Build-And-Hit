7338041999d2622db67727dcf01cf2dd33947e08
trumpool <3210105708@zju.edu.cn>
2023-07-24 01:41:32 +0800

    关掉了作弊码

--- Assets/Scripts/Block/BlockLIstManagerDeveloperSupport.cs
+++ Assets/Scripts/Block/BlockLIstManagerDeveloperSupport.cs
@@ -4,7 +4,7 @@ public partial class BlockListManager
 {
     KeyCode mTotallyDestroyKey, mDyeTowerSameColorKey /*red for now*/, mSpawnRedBlockKey, mSpawnBlueBlockKey, mSpawnGreenBlockKey, mSpawnSlimeBlockKey;
     private int DyeColor = 0;
-    public bool DeveloperMode { get; private set; } = true;       
+    public bool DeveloperMode { get; private set; } = false;       
 
     private void BindDeveloperKey()
     {
