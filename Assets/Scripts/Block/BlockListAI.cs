c4379f4971306873fbb4c7253e903cb58808e6de
trumpool <3210105708@zju.edu.cn>
2023-07-23 20:33:54 +0800

    [feat]: finish AI, ready to test

--- Assets/Scripts/Block/BlockListAI.cs
+++ Assets/Scripts/Block/BlockListAI.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public partial class BlockListManager
@@ -30,6 +32,75 @@ public partial class BlockListManager
     private int AIAttackTarget()
     {
         int vision = curPlayer.VisionRange();
-        return Random.Range(0, vision);
+        int BestHit = -1;
+        for(int i = 1; i < vision; i++)
+        {
+        }
+        if(BestHit != -1)
+        {
+            return BestHit;
+        }
+        else
+        {
+            return Random.Range(0, vision);
+        }
+    }
+
+    private int CalculateDestroyByHitThisBlock(int index)
+    {
+        int total = 1;
+        int lower = index - 1;
+        List<int> destroyList = new List<int>();
+        BlockManager blockManager = mBlockManagers[1 - mPlayerIndex];
+        for (int i = 0; i < blockManager.GetHeight(); i++)
+        {
+            destroyList.Add((int)blockManager.GetBlockColorAt(i));
+        }
+        //calculate distinct destroy list
+        int temp = index;
+        while (index > 0 && destroyList[index] == destroyList[index - 1])
+        {
+            index--;
+            total++;
+        }
+        int LowerBound = index;
+        index = temp;
+        while(index < destroyList.Count - 1 && destroyList[index] == destroyList[index + 1])
+        {
+            index++;
+            total++;
+        }
+        for (int i = 0; i < total; i++)
+        {
+            destroyList.RemoveAt(LowerBound);
+        }
+        //calculate cobol destroy
+        int ComboLower = LowerBound, ComboUpper = LowerBound + 1;
+        int ComboTotal = 0;
+        while (ComboLower >= 0 && ComboUpper < destroyList.Count && destroyList[ComboLower] == destroyList[ComboUpper])
+        {
+            int ComboInThisRound = 2;
+            while (ComboLower > 0 && destroyList[ComboLower] == destroyList[ComboLower - 1])
+            {
+                ComboLower--;
+                ComboInThisRound++;
+            }
+            while (ComboUpper <= destroyList.Count - 2 && destroyList[ComboUpper] == destroyList[ComboUpper + 1])
+            {
+                ComboUpper++;
+                ComboInThisRound++;
+            }
+
+            if (ComboInThisRound < 3)
+            {
+                break;// not enough to combo, end of all combo
+            }
+            ComboTotal += ComboInThisRound;
+        }
+        if(ComboTotal > 2)
+        {
+            total += ComboTotal;
+        }
+        return total;
     }
 }
